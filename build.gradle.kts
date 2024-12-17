a29e87c17519c5265e600f009107d957cf10a73d
Tim Van Meerbeeck <timaar@gmail.com>
2024-12-17 13:38:37 +0100

    bug() fixed build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
     kotlin("jvm") version "2.0.21"
 }
@@ -17,15 +15,17 @@ tasks.test {
 }
 
 dependencies {
-    val junitVersion = "3.26.3"
+    val junitVersion = "5.10.0"
+    val assertjVersion = "3.24.2"
+
     testImplementation(kotlin("test-junit5"))
     testImplementation("org.junit.jupiter:junit-jupiter:$junitVersion")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
-    testImplementation("org.assertj:assertj-core:$junitVersion")
-
-    testCompileOnly("org.junit.jupiter:junit-jupiter-api:$junitVersion")
-    testCompileOnly("org.junit.jupiter:junit-jupiter-params:$junitVersion")
+    testImplementation("org.assertj:assertj-core:$assertjVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
 }
+
+
 
 extensions.findByName("buildScan")?.withGroovyBuilder {
     setProperty("termsOfServiceUrl", "https://gradle.com/terms-of-service")
